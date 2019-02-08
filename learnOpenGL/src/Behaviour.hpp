48f98281204f24b40417070eb19119bba756f0c2
yanagiragi <qaz90473@gmail.com>
2019-02-09 05:46:55 +0800

    cleanup

--- learnOpenGL/src/Behaviour.hpp
+++ learnOpenGL/src/Behaviour.hpp
@@ -11,35 +11,7 @@
 class Behaviour 
 {
 	public:
-
-		const char* vertexShaderSource = "#version 330 core\n"
-			"layout (location = 0) in vec3 aPos;\n"
-			"void main()\n"
-			"{\n"
-			"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-			"}\0";
-
-		const char* fragmentShaderSource = "#version 330 core\n"
-			"out vec4 FragColor;\n"
-			"uniform vec4 ourColor;\n"
-			"void main()\n"
-			"{\n"
-			"   FragColor = ourColor;\n"
-			"}\n\0";
-
 		unsigned int VAO, VBO, EBO;
-		const float vertices[12] = {
-			0.5f,  0.5f, 0.0f,  // top right
-			0.5f, -0.5f, 0.0f,  // bottom right
-			-0.5f, -0.5f, 0.0f,  // bottom left
-			-0.5f,  0.5f, 0.0f   // top left 
-		};
-
-		const unsigned int indices[6] = {
-			0, 1, 3,
-			1, 2, 3
-		};
-
 		unsigned int vertexShader;
 		unsigned int fragmentShader;
 		unsigned int shaderProgram;
